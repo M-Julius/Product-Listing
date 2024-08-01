dca9fc58747695c9e66c3c41cb79d1baaebd842d
M-Julius <mjulius.dev@gmail.com>
2024-08-01 22:29:09 +0700

    add icon arrow, left, cart, close, minus, plus

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+export default {
+    cart: require('./cart.png'),
+    plus: require('./plus.png'),
+    minus: require('./minus.png'),
+    arrowleft: require('./arrow-left.png'),
+    close: require('./close.png'),
+};
